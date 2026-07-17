6ace0119b960c4088af6f2d66938d4aa94148897
Vitor Botelho <vitor.bot02@outlook.com>
2026-07-16 22:01:27 -0300

    feat: cria pastas e toda estrutura do core do projeto

---
+++ GestorTarefasAPI/Core/Interfaces/Repository/ITarefaRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using Core.Enums;
+
+namespace Core.Interfaces.Repository;
+
+public interface ITarefaRepository
+{
+    Task<Tarefa?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Tarefa>> GetAllAsync();
+    Task<IEnumerable<Tarefa>> GetByFilterAsync(Status? status, DateTime? dataVencimento);
+    Task AddAsync(Tarefa tarefa);
+    void Update(Tarefa tarefa);
+    void Remove(Tarefa tarefa);
+}
